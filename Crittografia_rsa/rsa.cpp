9b6931b9dd1ffadea42caaa068834e7f90f1313b
Jef <47726188+JekxDevil@users.noreply.github.com>
2019-11-27 01:38:14 +0100

    Update rsa.cpp
    Final solution. 100/100. Just keep thinking in binary, not decimal.

--- Crittografia_rsa/rsa.cpp
+++ Crittografia_rsa/rsa.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <iostream>
 
 static FILE *fr, *fw;
 
@@ -22,7 +23,7 @@ int main() {
 	int i0;
 
 	// Reading input
-        fscanf(fr, "%d %d %d", &N, &d, &L);
+    fscanf(fr, "%d %d %d", &N, &d, &L);
 	messaggio = (int*)malloc(100 * sizeof(int));
 	plaintext = (char*)malloc(110 * sizeof(char));
 	for (i0 = 0; i0 < L; i0++)
@@ -32,48 +33,34 @@ int main() {
 	decifra(N, d, L, messaggio, plaintext);
 
 	// Writing output
-        fprintf(fw, "%s\n", plaintext);
-	
+    fprintf(fw, "%s\n", plaintext);
+
 	fclose(fr);
 	fclose(fw);
 	return 0;
 }
 
-//(A + B) mod M = (A mod M + B mod M) mod M
-//(A · B) mod M = (A mod M · B mod M) mod M
 void decifra(int N, int d, int L, int* messaggio, char* plaintext)
 {	
-	//TODO change * -> + , power -> *
-	long long int A, B, power_a;
-	bool isOdd;
+	unsigned long long int crypto, result;
 
-	isOdd = d % 2 == 1 ? true : false;
-	power_a = d >> 1;
+	//itering through crypto numbers in array
+	for(unsigned long long int index=0; index<L; index++){
+			crypto = messaggio[index];
+			result = 1;
 
-	//itering through cripto numbers in array
-	for(long long int i=0; i<L; i++){
-		A = 1;
-		B = 1;
+			while(d > 0){
 
-		//(A · B) mod M = (A mod M · B mod M) mod M
-		for(int num_potenza = 0; num_potenza <= power_a; num_potenza++){
-			
-			if(num_potenza == power_a){
-				
-				//power of b
-				if(isOdd){
-					B = A % N * messaggio[i] % N;
-				} else {
-					B = A;
-				}
-				
-			} else {
-				A = A % N * messaggio[i] % N;
+				//current odd exponential
+				if(d & 1)
+					result = result % N * crypto % N;
+
+				crypto = crypto % N * crypto % N;
+				d >>= 1;
 			}
-		}
 
-		//decrypted char assignment
-		plaintext[i] = A % N * B % N;
+		//decrypted char assignment 
+		plaintext[index] = result;
 	}
 
 	plaintext[L] = '\0';
