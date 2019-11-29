5a04f026db1aa6272be843dc41d09249b6aaabef
Jef <jekx123devil@gmail.com>
2019-11-29 17:20:09 +0100

    Possible solution. Need to improve.
    It's a faster solution. Need to verify the output to be correct.

--- lotteria_quadri/lotteria.cpp
+++ lotteria_quadri/lotteria.cpp
@@ -24,16 +24,55 @@ int quadri(int N, long long M, int* V)
     //avanzare per partire dal massimo in giu'
     for(; length<N; length++){
         sum+=V[length];
-        if(sum>M)
+        if(sum>M){
+	    length--;	
             break;
+	}
     }
 
-    //lunghezza massima uguale array
+    //lunghezza massima uguale array o 0 possibili contenuti
     if(length == N)
         return length;
+    else if(length <= 0)
+	return 0;
 
     //retrocedere
-    
+    bool found;
+
+    //iterare diminuendo lunghezza a ogni ciclo
+    for(long long _spanLimit=0;length>0;length--, _spanLimit++){
+	//printf("\n [Entro dentro ciclo tosto] \n");
+	//diamo per scontato che ogni ciclo sia il massimo ottenibile
+        found = true;
+
+	//sommiamo tutte le possibili combinazioni di della lunghezza attuale
+	for(long long pivot=0; pivot<_spanLimit+1; pivot++){
+	    for(long long span=0; span<_spanLimit+1; span++){
+	        sum = V[pivot];
+	        
+		for(long long slider=pivot+span+1; slider<N && slider-pivot<length; slider++){
+		    sum += V[slider];
+		}
+
+		//array cattivo, esci e diminuisci lunghezza
+		if(sum>M){
+		    found=false;
+		    break;
+		}
+	    }	
+
+	    //se un caso eccede, si esce dal ciclo e si diminuisce la lunghezza
+	    if(!found){
+		break;
+	    }
+	}
+	
+	//se tutti i casi buoni, massimale trovato, esco da ciclo
+	if(found){
+	   // printf("break caso buono: length = %d \n",length);
+	    break;
+	}
+    }   
 
     return length;
 }
