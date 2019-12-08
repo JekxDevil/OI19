22f7b8f7c35dae3ae6a7af098f590defdc624916
JekxDevil <jekx123devil@gmail.com>
2019-12-08 17:08:14 +0100

    Solution to refactor
    Still in dev. 30/100. Seems need to restructure the code.

--- refactor/refactor.cpp
+++ refactor/refactor.cpp
@@ -12,86 +12,108 @@ int main() {
     bool isShortComment = false, isLongComment = false, slashFlag = false, asteriscFlag = false, isString = false, isNewWord = false, isMacro = false, isCorrect = false;
 
     //  uncomment the following lines if you want to read/write from files
-    freopen("input1.txt", "r", stdin);
-    freopen("output1.txt", "w", stdout);
+    //freopen("input1.txt", "r", stdin);
+    //freopen("output1.txt", "w", stdout);
     assert(1 == scanf("%s", identifier));
     char c;
     
     //ascii reading
     while ((c = getchar()) != EOF) {
-//problemi con 2 testcase. asterisco o comunque operazioni con variabili
+
         if(isCorrect) {
             index++;
-            //printf("\n %c", identifier[index]);
 
-            if(identifier[index] == '\0') {//non si ferma quando incontra
+            if(identifier[index] == '\0') {
                 result++;
                 isCorrect = false;
             } else if(identifier[index] != c) {
                 isCorrect = false;
             }
         } else {
-            switch (c)
-            {
-            case ' ':
-                isNewWord = true;
-                break;
 
-            case '\n':
-                //short comment end
-                if(isShortComment) isShortComment = false;
-                if(isMacro) isMacro = false;
-                break;
-            
-            case '"':
-                if(!isString)
-                    isString = true;
-                else
-                    isString = false;
-                break;
+            switch (c) {
+                case ' ':
+                case '+':
+                case '-':
+                case '%':
+                case '!':
+                case '&':
+                case '|':
+                case '<':
+                case '>':
+                case '^':
+                case '~':
+                case '(':
+                case ')':
+                case '{':
+                case '}':
+                case '[':
+                case ']':
+                case '.':
+                case ';':
+                case '=':
+                case ':':
+                    isNewWord = true;
+                    break;
 
-            case '#':
-                isMacro = true;
-                break;
-
-            case '/':
-                if(!slashFlag) 
-                    slashFlag = true;
-                else 
-                    //short comment start
-                    isShortComment = true;
-
-                //longcomment end
-                if(asteriscFlag) isLongComment = false;
-                break;
-
-            case '*':
-                //longcomment start
-                if(slashFlag) isLongComment = true;
-                asteriscFlag = true;
-                break;
-
-            default:
-                if(slashFlag) slashFlag = false;
-                if(asteriscFlag) asteriscFlag = false;
+                case '\n':
+                    //short comment end
+                    if(isShortComment) isShortComment = false;
+                    if(isMacro) isMacro = false;
+                    isNewWord = true;
+                    break;
                 
-                if(!isShortComment && !isLongComment && !isMacro && !isString && isNewWord) {
-                    isNewWord = false;
-                    index = 0;
-                    
-                    if(identifier[index] == c) isCorrect = true;
-                }
+                case '"':
+                    if(!isString) {
+                        isString = true;
+                    } else {
+                        isString = false;
+                    }
+                    break;
 
-                break;
+                case '#':
+                    if(!isString) isMacro = true;
+                    break;
+
+                case '/':
+
+                    if(!slashFlag) {
+                        slashFlag = true;
+                    } else if(!isString && !isLongComment){//already flag true
+                        isShortComment = true;
+                    } else if(!isString && isLongComment && asteriscFlag) {
+                        isLongComment = false;
+                    }
+                    
+                    isNewWord = true;
+                    break;
+
+                case '*':
+                    //longcomment start
+                    if(slashFlag && !isString) isLongComment = true;
+                    asteriscFlag = true;
+                    isNewWord = true;
+                    break;
+
+                default:
+
+                    slashFlag = false;
+                    asteriscFlag = false;
+                    
+                    if(!isShortComment && !isLongComment && !isMacro && !isString && isNewWord) {
+                        
+                        if(identifier[0] == c) {
+                            index = 0;
+                            isCorrect = true;
+                        }
+                    }
+
+                    isNewWord = false;
+                    break;
             }
         }
     }
     
-    printf("%d\n", result); // print the result
+    printf("%lld\n", result); // print the result
     return 0;
 }
-
-/*
-0 = 3
-1 = 2
-*/
